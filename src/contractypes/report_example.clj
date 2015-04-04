b9735e654f61ab1ed05375dc2c17cc7ff717c7d0
Jessica Kerr <jessitron@outpace.com>
2015-04-04 01:55:33 +0000

    add stubby analyze method

--- src/contractypes/report_example.clj
+++ src/contractypes/report_example.clj
@@ -12,10 +12,26 @@
 (defn format-report [report-data]
   { :cells [[(:title report-data)]]})
 
+(declare group-up summarize add-total-row add-headers)
+
 (defn analyze-ad-performance [events params]
-  (select-keys params [:title]))
+  (-> events
+      (group-up params)
+      summarize
+      add-total-row
+      (add-headers params)))
 
+(defn add-headers [so-far params]
+  (merge so-far (select-keys params [:title])))
 
+(defn add-total-row [groups]
+  {:groups groups})
+
+(defn summarize [so-far]
+  so-far)
+
+(defn group-up [events params]
+    [events])
 
 (defn fetch-events [params]
   [{:who "me" :what "click" :when (time/now)}])
