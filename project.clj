04f9cc78b4689b9aa93a2513787732db6218bdde
Jessica Kerr <jessitron@outpace.com>
2015-04-07 10:52:02 +0000

    Add schematron for defgen

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.7.0"]
                  [prismatic/schema "0.3.7"]
-                 [clj-time "0.9.0"]])
+                 [clj-time "0.9.0"]
+                 [schematron "0.1.0-SNAPSHOT"]])
--- test/contractypes/generators.clj
+++ test/contractypes/generators.clj
@@ -2,9 +2,12 @@
   (:require [clojure.test.check.generators :as gen]
             [contractypes.libbit.datetime-generators :as time-gen]
             [contractypes.schemas :as t]
-            [clj-time.core :as time]))
+            [schematron.core :as st]
+            [contractypes.schemas :as t]
+            [clj-time.core :as time]
+            [schema.core :as s]))
 
-(def params (gen/hash-map :title gen/string-alpha-numeric
+(st/defgen params t/Params (gen/hash-map :title gen/string-alpha-numeric
                           :start (time-gen/datetime-before (time/now))
                           :end (time-gen/datetime-before (time/now))))
 ;; TODO: bring in the monad and define start-date in terms of end
